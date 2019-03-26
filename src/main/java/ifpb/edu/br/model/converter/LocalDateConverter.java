572b27050986aa0e4ac0324db56267f7c7a89ff2
Amanda Gomes <amandagomes2010@gmail.com>
2019-03-26 17:26:08 -0300

    login estatico

---
+++ src/main/java/ifpb/edu/br/model/converter/LocalDateConverter.java
@@ -0,0 +1,29 @@
+package ifpb.edu.br.model.converter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author Amanda
+ */
+@FacesConverter(value="converter.LocalDate")
+public class LocalDateConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return LocalDate.parse(value, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        LocalDate dateValue = (LocalDate) value;
+        return dateValue.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+    }
+    
+}
